a16347b60b3579f21f1d8d027f6cc11ef32fd4e8
dingyinghao <942677104@qq.com>
2025-09-01 10:08:23 +0800

    chore: 更新导航数据 - 2025/9/1 10:08:22

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -482,6 +482,6 @@ export const mockData = {
     }
   ],
   "title": "猫猫导航",
-  "search": "bing",
-  "_fileSha": "cf9e9f1ad8644add6c0918786cae6707de255aaf"
+  "search": "baidu",
+  "_fileSha": "3e32499fd88fe787537b9ce08b18612375673d4f"
 }
